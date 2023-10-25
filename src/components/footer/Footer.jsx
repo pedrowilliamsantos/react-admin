9d368d86c1304d92b9cb894606cac9cb671f2b2f
pedrowilliamsantos <pedrowilliam866@gmail.com>
2023-10-25 17:03:39 -0300

    adicionando o footer na pagina

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./footer.scss"
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <span>React Admin</span>
+      <span>© React Admin DashBoard</span>
+    </div>
   )
 }
 
