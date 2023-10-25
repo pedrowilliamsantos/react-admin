b31aba3757fb9591dd3499900efe836e50f7a826
pedrowilliamsantos <pedrowilliam866@gmail.com>
2023-10-25 17:29:12 -0300

    populando a primeira box

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./home.scss"
+import Topbox from "../../components/topBox/Topbox"
 
 const Home = () => {
-  return (
-    <div className='home'>Home</div>
-  )
+    return (
+        <div className='home'>
+            <div className="box box1">
+                <Topbox/>    
+            </div>
+            <div className="box box2">box </div>
+            <div className="box box3">box </div>
+            <div className="box box4">box </div>
+            <div className="box box5">box </div>
+            <div className="box box6">box </div>
+            <div className="box box7">box </div>
+            <div className="box box8">box </div>
+            <div className="box box9">box </div>
+        </div>
+    )
 }
 
 export default Home
